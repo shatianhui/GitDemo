0a4b6c29933456acd44f2f73815a92334331dd34
Sha Tianhui <3272027591@qq.com>
2022-07-05 19:40:17 +0800

    first commit

---
+++ src/main/java/hdu/sth/git.java
@@ -0,0 +1,13 @@
+package hdu.sth;
+
+/**
+ * description: git <br>
+ * date: 2022/7/5 19:36 <br>
+ * author: shatianhui <br>
+ * version: 1.0 <br>
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
